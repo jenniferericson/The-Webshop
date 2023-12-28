474caa8c733fc515076d18ce8fafb0a8012a3506
jenniferericson <jenniferericson813@gmail.com>
2023-12-28 13:41:10 +0100

    started to style our products page

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,11 +1,10 @@
 import { IProduct } from "../models/IProduct";
 import { get } from "./serviceBase";
 
-export const getProducts = async ():Promise <IProduct> => {
+export const getProducts = async ():Promise <IProduct[]> => {
     const url = "https://fakestoreapi.com/products/category/women's%20clothing";
-    const data = await get<IProduct>(url);
+    const data = await get<IProduct[]>(url);
 
-    
     return data;
 };
 
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,11 +1,31 @@
 import { getProducts } from "../services/productService";
 import "./../scss/style.scss";
 
-const products = await getProducts()
+const productsContainer = document.getElementById("productsContainer");
 
-console.log(products)
+const products = await getProducts();
 
+for (let i = 0; i < products.length; i++) {
+  const productBox = document.createElement("div");
+  const img = document.createElement("img");
+  const title = document.createElement("h2");
+  const price = document.createElement("p");
 
+  productBox.className = ("productBox");
+  img.className = ("productBox--img");
+  title.className = ("productBox--title");
+  price.className = ("productBox--price");
+
+  img.src = products[i].image;
+  title.innerHTML = products[i].title;
+  price.innerHTML = products[i].price +" $".toString();
+  
+  productBox.appendChild(img);
+  productBox.appendChild(title);
+  productBox.appendChild(price);
+
+  productsContainer?.appendChild(productBox);
+}
 
 
 
