3026e1d34e5b1a29895ca76e6a8a687d41ce7d03
Leonard Deutgen <leonard.deutgen@medieinstitutet.se>
2023-12-28 16:19:09 +0100

    added to cart function

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,6 +3,9 @@ import "./../scss/style.scss";
 
 const productsContainer = document.getElementById("productsContainer");
 
+let cartValue:number = 1;
+const cartValueTag = document.getElementById("cartValueTag") as HTMLElement;
+
 const products = await getProducts();
 
 for (let i = 0; i < products.length; i++) {
@@ -10,22 +13,33 @@ for (let i = 0; i < products.length; i++) {
   const img = document.createElement("img");
   const title = document.createElement("h2");
   const price = document.createElement("p");
+  const addToCartBtn = document.createElement("button");
 
   productBox.className = ("productBox");
   img.className = ("productBox--img");
   title.className = ("productBox--title");
   price.className = ("productBox--price");
+  addToCartBtn.className = ("productBox--btn");
 
   img.src = products[i].image;
   title.innerHTML = products[i].title;
   price.innerHTML = products[i].price +" $".toString();
+  addToCartBtn.innerHTML = "Add to cart"
   
   productBox.appendChild(img);
   productBox.appendChild(title);
   productBox.appendChild(price);
-
+  productBox.appendChild(addToCartBtn);
   productsContainer?.appendChild(productBox);
+
+  addToCartBtn.addEventListener("click", ()=>{
+    cartValueTag.innerHTML = "";
+    cartValueTag.innerHTML = cartValue.toString();
+    cartValue +=1;
+  })
 }
+
+
 
 
 
