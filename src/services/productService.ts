9522666c63c6aec238d829cc5c3e03aded277383
jenniferericson <jenniferericson813@gmail.com>
2023-12-28 12:27:38 +0100

    set up models and services, and added our api

---
+++ src/services/productService.ts
@@ -0,0 +1,13 @@
+import { IProduct } from "../models/IProduct";
+import { get } from "./serviceBase";
+
+export const getProducts = async ():Promise <IProduct> => {
+    const url = "https://fakestoreapi.com/products/category/women's%20clothing";
+    const data = await get<IProduct>(url);
+
+    
+    return data;
+};
+
+
+
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,13 @@
+import { getProducts } from "../services/productService";
 import "./../scss/style.scss";
+
+const products = await getProducts()
+
+console.log(products)
+
+
+
+
+
+
+
